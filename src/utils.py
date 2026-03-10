2f510b7a1c58762baee09bd6c51548cd2db533c1
Pankajthakur544 <pankajthakur0000121@gmail.com>
2026-03-10 22:28:37 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -1,48 +1,59 @@
 import os
 import sys
 
-
 import numpy as np
 import pandas as pd
-import dill 
+import dill
+
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
-def save_object(file_path,obj):
+
+def save_object(file_path, obj):
     try:
-        dir_path =os.path.dirname(file_path)
+        dir_path = os.path.dirname(file_path)
 
-        os.makedirs(dir_path,exist_ok=True)
+        os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path,"wb") as file_obj:
-            dill.dump(obj,file_obj)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e,sys)
-    
-def evaluate_models(X_train,y_train,X_test,y_test, models):
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
 
-        report={}
+        report = {}
 
-        for i in range(len(list(models))):
-            model=list(models.values())[i]
+        for i in range(len(models)):
 
-            model.fit(X_train,y_train) #Train model
+            model = list(models.values())[i]
 
-            y_train_pred=model.predict(X_train)
+            para = params[list(models.keys())[i]]
 
-            y_test_pred=model.predict(X_test)
+            gs = GridSearchCV(model, para, cv=3)
 
-            train_model_score=r2_score(y_train,y_train_pred)
+            gs.fit(X_train, y_train)
 
-            test_model_score=r2_score(y_test,y_test_pred)
+            model.set_params(**gs.best_params_)
 
-            report[list(models.keys())[i]]=test_model_score
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
 
         return report
-    
-    except Exception as e:
-        raise CustomException(e,sys)
 
+    except Exception as e:
+        raise CustomException(e, sys)
